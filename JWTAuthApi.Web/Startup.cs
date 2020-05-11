518aa50a982c60cb075d75df7134fb1b32e14edb
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-11 22:20:21 +0200

    JAA-3: Register services in Startup

--- JWTAuthApi.Web/Startup.cs
+++ JWTAuthApi.Web/Startup.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JWTAuthApi.Core.Interfaces;
+using JWTAuthApi.Data.Context;
+using JWTAuthApi.Data.Repositories;
+using JWTAuthApi.Services.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +30,13 @@ namespace JWTAuthApi.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IUserRepository, UserRepository>();
+
+            services.AddTransient<IUserService, UserService>();
+
             services.AddControllers();
         }
 
