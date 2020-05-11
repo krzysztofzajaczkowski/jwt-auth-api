d0b2e04888ce534bc8be850f53b13e7eb1f35460
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-11 22:14:36 +0200

    JAA-3: Add IUserService interface

---
+++ JWTAuthApi.Core/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JWTAuthApi.Core.Models;
+
+namespace JWTAuthApi.Core.Interfaces
+{
+    public interface IUserService
+    {
+        Task AddAsync(User user);
+        Task<User> GetByIdAsync(int userId);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task UpdateAsync(User updatedUser);
+        Task DeleteAsync(int userId);
+    }
+}
