baa867e100d3a9de586a7a9d29fddaa40c818675
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-11 22:19:43 +0200

    JAA-3: Add IUserService implementation

---
+++ JWTAuthApi.Services/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JWTAuthApi.Core.Interfaces;
+using JWTAuthApi.Core.Models;
+
+namespace JWTAuthApi.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _userRepository.AddAsync(user);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int userId)
+        {
+            return await _userRepository.GetByIdAsync(userId);
+        }
+
+        public async Task UpdateAsync(User updatedUser)
+        {
+            await _userRepository.UpdateAsync(updatedUser);
+        }
+
+        public async Task DeleteAsync(int userId)
+        {
+            await _userRepository.DeleteAsync(userId);
+        }
+    }
+}
