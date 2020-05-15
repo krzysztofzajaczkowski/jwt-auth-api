bd1e225ad1c225f6ed2c200768074fa73a060982
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-15 20:13:07 +0200

    JAA-4: Add UsersController to web layer

---
+++ JWTAuthApi.Web/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWTAuthApi.Core.Interfaces;
+using JWTAuthApi.Core.Models;
+using JWTAuthApi.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthApi.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+    }
+}
