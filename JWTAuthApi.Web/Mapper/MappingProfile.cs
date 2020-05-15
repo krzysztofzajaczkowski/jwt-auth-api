4e7f3aa86e1f48028ee67e1903613edc060a9fc2
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-15 22:47:00 +0200

    JAA-5: Add MappingProfile

---
+++ JWTAuthApi.Web/Mapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using JWTAuthApi.Core.Models;
+using JWTAuthApi.Web.Dtos.UserDtos;
+
+namespace JWTAuthApi.Web.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDetailsDto>();
+            CreateMap<UserUpdateDto, User>();
+            CreateMap<UserRegisterDto, User>();
+            CreateMap<UserLoginDto, User>();
+        }
+    }
+}
