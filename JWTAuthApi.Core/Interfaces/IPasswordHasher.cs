14867f188a165da1e7b681c8ef87141aa2f77eda
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 00:15:12 +0200

    JAA-8: Add IPasswordHasher interface

---
+++ JWTAuthApi.Core/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthApi.Core.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string hash, string password);
+    }
+}
