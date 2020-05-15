fa2fd6435f70d86cbd5cf6d9b5ca8c08ab2e6589
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-15 23:49:39 +0200

    JAA-6: Register JWT based authentication and authorization in Startup

--- JWTAuthApi.Web/Startup.cs
+++ JWTAuthApi.Web/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using JWTAuthApi.Core.Helpers;
 using JWTAuthApi.Core.Interfaces;
 using JWTAuthApi.Data.Context;
 using JWTAuthApi.Data.Repositories;
 using JWTAuthApi.Services.Services;
 using JWTAuthApi.Web.Mapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -17,6 +20,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JWTAuthApi.Web
 {
@@ -41,7 +45,30 @@ namespace JWTAuthApi.Web
 
             services.AddAutoMapper(typeof(MappingProfile));
 
+            services.AddCors();
             services.AddControllers();
+
+            var JWTAuthSettingsSection = Configuration.GetSection("JWTAuthSettings");
+            services.Configure<JWTAuthSettings>(JWTAuthSettingsSection);
+
+            var JWTAuthSettings = JWTAuthSettingsSection.Get<JWTAuthSettings>();
+            var key = Encoding.ASCII.GetBytes(JWTAuthSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +82,13 @@ namespace JWTAuthApi.Web
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
