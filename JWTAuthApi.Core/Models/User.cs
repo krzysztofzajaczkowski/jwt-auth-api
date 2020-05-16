36867dae0038fc853f389070f8d98513714ca354
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 13:08:30 +0200

    JAA-9: Add List of UserRole to User model

--- JWTAuthApi.Core/Models/User.cs
+++ JWTAuthApi.Core/Models/User.cs
@@ -1,11 +1,18 @@
-﻿namespace JWTAuthApi.Core.Models
+﻿using System.Collections.Generic;
+
+namespace JWTAuthApi.Core.Models
 {
     public class User
     {
+        public User()
+        {
+            Roles = new List<UserRole>();
+        }
         public int Id { get; set; }
         public string Username { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
         public string Token { get; set; }
+        public List<UserRole> Roles { get; set; }
     }
 }
