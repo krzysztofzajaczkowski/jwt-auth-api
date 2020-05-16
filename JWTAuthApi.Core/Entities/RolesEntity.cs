7bc3d8a5db7dbd3b200093fc16e9e7d630022de4
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 13:33:48 +0200

    JAA-9: Add static RolesEntity with available roles

---
+++ JWTAuthApi.Core/Entities/RolesEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTAuthApi.Core.Entities
+{
+    public static class RolesEntity
+    {
+        public const string Admin = "Admin";
+        public const string Staff = "Staff";
+        public const string User = "User";
+    }
+}
