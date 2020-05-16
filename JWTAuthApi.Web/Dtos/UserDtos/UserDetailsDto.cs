41d68bf038db3bdd1f3a82dc704afeace4f7f813
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 13:30:18 +0200

    JAA-9: Edit UserDetails DTO to use UserRoleDto

--- JWTAuthApi.Web/Dtos/UserDtos/UserDetailsDto.cs
+++ JWTAuthApi.Web/Dtos/UserDtos/UserDetailsDto.cs
@@ -1,4 +1,7 @@
-﻿namespace JWTAuthApi.Web.Dtos.UserDtos
+﻿using System.Collections.Generic;
+using JWTAuthApi.Core.Models;
+
+namespace JWTAuthApi.Web.Dtos.UserDtos
 {
     public class UserDetailsDto
     {
@@ -7,6 +10,7 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string Token { get; set; }
+        public List<UserRoleDto> Roles { get; set; }
 
     }
 }
