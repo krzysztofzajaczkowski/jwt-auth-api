506e8bcc3cc62b7fca9cf1c386d2ed261ffff71f
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 13:31:36 +0200

    JAA-9: Add UserPreviewDto for basic user info

---
+++ JWTAuthApi.Web/Dtos/UserDtos/UserPreviewDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace JWTAuthApi.Web.Dtos.UserDtos
+{
+    public class UserPreviewDto
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public List<UserRoleDto> Roles { get; set; }
+
+    }
+}
