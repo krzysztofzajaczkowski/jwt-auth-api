994754edb74f3811671eaf86596bda221851a683
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-16 22:30:08 +0200

    JAA-10: Add AccessLevelRequirement and AuthorizationHandler

---
+++ JWTAuthApi.Web/Policies/AccessLevelRequirement.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JWTAuthApi.Web.Policies
+{
+    public class AccessLevelRequirement : IAuthorizationRequirement
+    {
+        public int MinimumAccessLevel { get; set; }
+        public AccessLevelRequirement(int minimumAccessLevel)
+        {
+            MinimumAccessLevel = minimumAccessLevel;
+        }
+    }
+}
